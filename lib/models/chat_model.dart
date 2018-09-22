10b73136ad246b1ccffe9e72c8637463aa36ae4c
SathishSaminathan <Sathishcse1975@gmail.com>
2018-09-22 09:58:57 +0530

    chat_list_view implemented

---
+++ lib/models/chat_model.dart
@@ -0,0 +1,34 @@
+class ChatModel{
+  final String name;
+  final String message;
+  final String time;
+  final String avatarUrl;
+
+  ChatModel({this.name,this.message,this.time,this.avatarUrl});
+}
+
+List<ChatModel> dummyData = [
+  new ChatModel(
+    name: "Sathish",
+    message: "Hai Good Morning!!!",
+    time: "15.30",
+    avatarUrl:"https://media.licdn.com/dms/image/C5103AQGwIzOfNXlK-Q/profile-displayphoto-shrink_200_200/0?e=1539216000&v=beta&t=1j1uU1rxQKV1q3tvOyhAfX-fC3ajvuF4FR_B76L__fM"
+  ),
+   new ChatModel(
+    name: "Saminathan",
+    message: "Hai Good Morning!!!",
+    time: "15.30",
+    avatarUrl:"https://media.licdn.com/dms/image/C5103AQGwIzOfNXlK-Q/profile-displayphoto-shrink_200_200/0?e=1539216000&v=beta&t=1j1uU1rxQKV1q3tvOyhAfX-fC3ajvuF4FR_B76L__fM"
+  ),
+   new ChatModel(
+    name: "Keethu",
+    message: "Hai Good Morning!!!",
+    time: "15.30",
+    avatarUrl:"https://media.licdn.com/dms/image/C5103AQGwIzOfNXlK-Q/profile-displayphoto-shrink_200_200/0?e=1539216000&v=beta&t=1j1uU1rxQKV1q3tvOyhAfX-fC3ajvuF4FR_B76L__fM"
+  ), new ChatModel(
+    name: "Sachu",
+    message: "Hai Good Morning!!!",
+    time: "15.30",
+    avatarUrl:"https://media.licdn.com/dms/image/C5103AQGwIzOfNXlK-Q/profile-displayphoto-shrink_200_200/0?e=1539216000&v=beta&t=1j1uU1rxQKV1q3tvOyhAfX-fC3ajvuF4FR_B76L__fM"
+  )
+];
--- lib/whatsapp_home.dart
+++ lib/whatsapp_home.dart
@@ -41,6 +41,13 @@ class _WhatsAppHomeState extends State<WhatsAppHome>
                 text: "CALLS",
               ),
             ]),
+            actions: <Widget>[
+              new Icon(Icons.search),
+              new Padding(
+                 padding:  const EdgeInsets.symmetric(horizontal: 5.0),
+              ),
+              new Icon(Icons.more_vert)
+            ],
       ),
       body: new TabBarView(
          controller:  _tabController,
